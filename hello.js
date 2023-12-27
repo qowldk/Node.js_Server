97b443bdbdaec43a87e25ece49568c654ea798ed
BaeJia <124412137+BaeJia@users.noreply.github.com>
2023-12-27 21:04:14 +0900

    간단한 라우터

--- hello.js
+++ hello.js
@@ -1,20 +1,23 @@
 const http = require("http"); // http 객체 생성
+const url = require("url"); // url 모듈을 로딩
 
-let count = 0;
+http
+  .createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname; // 패스명 할당
+    res.setHeader("Content-Type", "text/html");
 
-// 노드 서버 객체 생성
-const server = http.createServer((req, res) => {
-  log(count);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.write("hello\n");
-  setTimeout(() => {
-    res.end("Node.js");
-  }, 2000);
-});
-
-function log(count) {
-  console.log((count += 1));
-}
-
-server.listen(8000, () => console.log("Hello Node.js")); // 접속 대기
+    if (path === "/user") {
+      res.end("[user] name : andy, age : 30"); // /user 결괏값 설정
+    } else if (path === "/feed") {
+      res.end(`<meta charset="UTF-8"><ul>
+            <li>picture1</li>
+            <li>picture2</li>
+            <li>picture3</li>
+          </ul>
+      `); // /feed에 대한 결괏값 설정
+    } else {
+      res.statusCode = 404;
+      res.end("404 page not found"); // 결과값으로 에러 메세지 설정
+    }
+  })
+  .listen(3000, () => console.log("라우터를 만들어보자"));
