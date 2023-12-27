8ba2464dbabaebc31e8868325bd55a686c3eb2f0
BaeJia <124412137+BaeJia@users.noreply.github.com>
2023-12-27 21:18:33 +0900

    createServer() 리팩토링

--- hello.js
+++ hello.js
@@ -1,23 +1,33 @@
-const http = require("http"); // http 객체 생성
-const url = require("url"); // url 모듈을 로딩
-
+const http = require("http");
+const url = require("url");
 http
   .createServer((req, res) => {
-    const path = url.parse(req.url, true).pathname; // 패스명 할당
+    const path = url.parse(req.url, true).pathname;
     res.setHeader("Content-Type", "text/html");
-
     if (path === "/user") {
-      res.end("[user] name : andy, age : 30"); // /user 결괏값 설정
+      user(req, res); // 1
     } else if (path === "/feed") {
-      res.end(`<meta charset="UTF-8"><ul>
-            <li>picture1</li>
-            <li>picture2</li>
-            <li>picture3</li>
-          </ul>
-      `); // /feed에 대한 결괏값 설정
+      feed(req, res); // 2
     } else {
-      res.statusCode = 404;
-      res.end("404 page not found"); // 결과값으로 에러 메세지 설정
+      notFound(req, res); // 3
     }
   })
-  .listen(3000, () => console.log("라우터를 만들어보자"));
+  .listen("3000", () => console.log("라우터를 만들어보자!"));
+
+const user = (req, res) => {
+  res.end(`[user] name : andy, age: 30`);
+};
+
+const feed = (req, res) => {
+  res.end(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+      </ul>
+      `);
+};
+
+const notFound = (req, res) => {
+  res.statusCode = 404;
+  res.end("404 page not found");
+};
