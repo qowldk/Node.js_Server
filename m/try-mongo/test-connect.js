b941c3a9735dcbc6ea60a42e014bbb0fec9783ec
BaeJia <qowldk1030@gmail.com>
2024-01-12 22:52:33 +0900

    mongodb connect 테스트

--- m/try-mongo/test-connect.js
+++ m/try-mongo/test-connect.js
@@ -1,28 +1,45 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://qowldk1030:<password>@cluster0.aulipmv.mongodb.net/?retryWrites=true&w=majority";
+const MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://qowldk1030:grhdwn00@cluster0.aulipmv.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
 
-async function run() {
+async function main() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // 커넥션을 생성하고 연결을 시도
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
+
+    console.log("MongoDB 접속 성공");
+
+    // test 데이터베이스의 person 컬렉션을 가져옴
+    const collection = client.db("test").collection("person");
+
+    // Document 하나 추가
+    await collection.insertOne({ name: "Andy", age: 30 });
+    console.log("document 추가 완료");
+
+    // Document 찾기
+    const documents = await collection.find({ name: "Andy" }).toArray();
+    console.log("찾은 document:", documents);
+
+    // Document 갱신하기
+    await collection.updateOne({ name: "Andy" }, { $set: { age: 31 } });
+    console.log("document 업데이트");
+
+    // 갱신된 Document 확인하기
+    const updatedDocuments = await collection.find({ name: "Andy" }).toArray();
+    console.log("갱신된 document :", updatedDocuments);
+
+    // Document 삭제하기
+    // await collection.deleteOne({ name: 'Andy' });
+    console.log("document 삭제");
+
+    // 연결 끊기
     await client.close();
+  } catch (err) {
+    console.error(err);
   }
 }
-run().catch(console.dir);
+
+main();
