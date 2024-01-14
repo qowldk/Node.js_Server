ca472469011a1e0cbedebae32a1b36a9563410d0
BaeJia <qowldk1030@gmail.com>
2024-01-14 21:34:42 +0900

    api 작성 전 setup

--- board/app.js
+++ board/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
-app.engine("handlebars", handlebars.engine()); // 템플릿 엔진으로 핸들바 등록
+app.engine(
+  "handlebars",
+  handlebars.create({
+    helpers: require("./configs/handlebars-helpers"),
+  }).engine
+); // 템플릿 엔진으로 핸들바 등록
 app.set("view engine", "handlebars"); // 웹 페이지 로드시 사용할 템플릿 엔진 설정
 app.set("views", __dirname + "/views"); // 뷰 디렉토리를 views로 설정
 
@@ -20,4 +25,10 @@ app.get("/detail/:id", async (req, res) => {
   });
 });
 
-app.listen(3000);
+const mongodbConnection = require("./configs/mongodb-connection");
+app.listen(3000, async () => {
+  console.log("Server started");
+  const mongoClient = await mongodbConnection();
+  collection = mongoClient.db().collection("post");
+  console.log("MongoDB connected");
+});
